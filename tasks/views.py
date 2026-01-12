b8f2950c17bf609a7c5be4edb5a22d56f42e35a7
Esan <EgEbner16@gmail.com>
2026-01-12 04:36:51 +0000

    Login pages and signup pages exist

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import SignupForm
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log in immediately
+            return redirect("home")
+    else:
+        form = SignupForm()
+    return render(request, "tasks/signup.html", {"form": form})
+
+@login_required
+def home():
+    pass
